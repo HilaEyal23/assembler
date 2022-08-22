ee94727a57bb8967eb838cab98af90d993d39eda
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-08-22 15:47:52 +0300

    Create operationTable.h

---
+++ operationTable.h
@@ -0,0 +1,123 @@
+#ifndef _OPERATIONTABLE_H
+
+#define _OPERATIONTABLE_H
+
+
+
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+#define NUM_OF_OPERATIONS 16
+
+
+
+#define FIRST_ADDRESS 1
+
+#define SECOND_ADDRESS 2
+
+#define THIRD_ADDRESS 3
+
+#define FOURTH_ADDRESS 4
+
+
+
+/*
+
+This function is checking if the received name of the operationTable is valid or not.
+
+@param name - tha name of the operationTable
+
+@return int - wether it found that operation
+
+*/
+
+int isOperationName(char *name);
+
+
+
+
+
+
+
+/*
+
+This function receives the operationName and if the name is valid it returns the Opcode of the valid name.
+
+@param name - tha name of the operationTable
+
+@return int of the Opcode
+
+*/
+
+int getOperationOpcode(char *name);
+
+
+
+
+
+
+
+/*
+
+This function receives the operationName and if the name is valid it returns the OperandNumber of the valid name.
+
+@param name - tha name of the operationTable
+
+@return int of the OperandNumber
+
+*/
+
+int getOperandNum(char *name);
+
+
+
+
+
+
+
+/*
+
+This function receives the operationName and if the name is valid it returns the sourceOperand of the valid name.
+
+@param name - tha name of the operationTable
+
+@param sourceOperand - the sourceOperand of the operationTable
+
+@return int of the sourceOperand
+
+*/
+
+int getSourceOperand(char *name);
+
+
+
+
+
+
+
+/*
+
+This function receives the operationName and if the name is valid it returns the destinationOperand of the valid name.
+
+@param name - tha name of the operationTable
+
+@param destinationOperand - the destinationOperand of the operationTable
+
+@return int of the destinationOperand
+
+*/
+
+int getDestinationOperand(char *name);
+
+
+
+
+
+
+
+
+
+#endif /*_OPERATIONTABLE_H*/
