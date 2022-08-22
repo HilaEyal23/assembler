3fe3811a08b57912b9d0e2d467562cc6f8a8510b
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-08-22 15:04:25 +0300

    Update firstPass.h

--- firstPass.h
+++ firstPass.h
@@ -1,19 +1,89 @@
 #ifndef _FIRSTPASS_H
+
 #define _FIRSTPASS_H
 
-#include "globals.h"
+
+
 #include "symbolList.h"
 
-void init_line(receivedLine *receivedLine);
-void  parse_input(FILE *ptrFile);
-void get_input(receivedLine *receivedLine);
+#include "operationTable.h"
 
-int is_data_ins();
-int errors_found();
-int update_symbols_list();
-int is_label_def();
-int is_ext_or_ent();
-int is_ext();
-int is_symbol_def();
+#include "structs.h"
+
+
+
+/*#include "extern_vars.h"*/
+
+
+
+/******MAIN FUNTIONS IN FIRST PASS******/
+
+void first_pass(FILE *fp, char *fileName);
+
+int get_cmd_IDX(char str[]);
+
+void get_command(char *p, char command[]);
+
+int get_operand_type(char *op);
+
+int get_direct_type(char *token, int lineNumber);
+
+int find_L(int address1, int address2);
+
+
+
+/******VALIDATION FUNCTIONS******/
+
+int validate_instruction_form(char *line, int labelFlag, int lineNumber, int *arg1, int *arg2);
+
+int validate_direct_form(char *line, int labelFlag, int lineNumber);
+
+int validate_input_form(char *line, int min_args);
+
+int validate_command(char *token, int lineNumber);
+
+
+
+/******PARSING FUNCTIONS******/
+
+void skip_command(char **p);
+
+void skip_spaces(char **p);
+
+int check_comma(char c);
+
+void skip_label(char **line);
+
+void skip_token(char **p);
+
+int skip_empty(char *line, int i);
+
+int to_be_skipped(char line[]);
+
+void copy_label_name(char *label, char *token);
+
+
+
+/******DETECTING FUNCTIONS******/
+
+int is_label(char *token, int lineNumber);
+
+int is_direct(char *token, int lineNumber);
+
+int is_number(char *num);
+
+int is_alpha_word(char *word);
+
+
+
+/******DEBUG TOOLS******/
+
+void print_cmdArray();
+
+void print_cmd_line(cmdLine line);
+
+
+
+
 
 #endif /*_FIRSTPASS_H*/
