c8427628e2492db924db73b7e6377ad76ffc4ccb
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-08-23 14:32:12 +0300

    fixed "\n" in macro.

---
+++ makefile
@@ -0,0 +1,11 @@
+FLAGS = -ansi -Wall -pedantic
+
+preprocessor: preprocessor.o macro_list.o utils.o
+	gcc -g $(FLAGS) preprocessor.o macro_list.o utils.o -o preprocessor 
+preprocessor.o: preprocessor.c
+	gcc -c $(FLAGS) preprocessor.c -o preprocessor.o 
+utils.o: utils.c
+	gcc -c $(FLAGS) utils.c -o utils.o 
+macro_list.o: macro_list.c
+	gcc -c $(FLAGS) macro_list.c -o macro_list.o
+
---
+++ preprocessor.c
@@ -0,0 +1,71 @@
+#include "preprocessor.h"
+
+int main(int argc, char **argv){
+    FILE *fp;
+    char filesName[MAX_LINE_LENGTH] = {0};
+
+    if(argc == 1){
+        printf("no files were given\n");
+        exit(0);
+    }
+    strcpy(filesName, argv[1]);
+    fp = fopen(filesName,"r"); /*opening the file according it's name (including file type)*/
+    preprocess(fp, filesName);
+    return 0;
+}
+
+void preprocess(FILE *fp, char *fileName){
+    char line[MAX_LINE_LENGTH];
+    char lineCopy[MAX_LINE_LENGTH];
+    char name[MAX_LINE_LENGTH];
+    char *token;
+    char *content;
+    macroNode *head;
+    int macroFlag = false;
+    char *p; 
+    int i = 1;
+
+    FILE *preprocessedFile = fopen(strcat(fileName, ".am"), "w");
+    head = init_macro_node(NULL, "999","999");
+    content = (char*)malloc(MAX_LINE_LENGTH * sizeof(char));
+
+    while(fgets(line, MAX_LINE_LENGTH, fp)){
+        strcpy(lineCopy, line);
+        token = strtok(lineCopy, " \t");
+        
+   
+        if(macro_exists(head, token)){
+            stream_macro(preprocessedFile, head, name);
+        }
+        else if(!strcmp(token, "macro")){
+            macroFlag = true;
+            token = strtok(NULL, " \t");
+            strcpy(name, token);
+        }
+        else if(macroFlag){
+            i++;
+            strcpy(lineCopy, line);
+            if(!strncmp(token,"endmacro", 8)){
+
+                macroFlag = false;
+                insert_at_end(&head, name, content);
+                
+                content = (char*)malloc(MAX_LINE_LENGTH * sizeof(char));
+                p = NULL;
+                i = 1;
+            }
+            else{        
+                p = (char*)realloc(content, MAX_LINE_LENGTH*sizeof(char)*i);  
+                if(p == NULL) printf("error with allocation!");
+                else content = p;         
+                strcat(content, lineCopy);
+            }
+        }
+        else{
+            fputs(line, preprocessedFile);
+        }
+    }
+
+    /*end_of_file();*/
+}
+
