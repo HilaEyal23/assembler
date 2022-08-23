3a3deaa9f7332619f538e641a851d7de8d528cc3
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-08-23 14:43:38 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -11,13 +11,7 @@
 #define false 0
 #define TAB '\t'
 
-int is_cmd(char str[]);
-int is_int(char s[]);
-/*void skipEmpty(char line[]);*/
-int toBeSkipped(char line[]);
-int skipEmpty(char *line, int i);
 void skip_spaces(char **p);
-int toBeSkipped(char line[]);
 int space_line(char *line);
 int comment_line(char *line);
 
